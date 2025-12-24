9cdd8fb8667c9b335d6f39af0ccde74f258fdd77
woojoo48 <wjddnwn0408@gmail.com>
2025-12-24 14:39:17 +0900

    Initial Commit

---
+++ src/main/java/com/umc/devine/DeVineApplication.java
@@ -0,0 +1,13 @@
+package com.umc.devine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeVineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeVineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc/devine/DeVineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.devine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeVineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
