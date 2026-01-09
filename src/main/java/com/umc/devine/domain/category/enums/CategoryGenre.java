d1ac778c5e5a1fadf87238ff258430bb31e3d954
strfunctionk <comzun01@gmail.com>
2026-01-10 02:33:43 +0900

    Feat(entity): Domain 추가
    - Category 엔티티 생성
    
    - CategoryGenre enum 생성
    
    - MemberCategory 엔티티 생성
    
    IssueNum: #8

---
+++ src/main/java/com/umc/devine/domain/category/enums/CategoryGenre.java
@@ -0,0 +1,11 @@
+package com.umc.devine.domain.category.enums;
+
+public enum CategoryGenre {
+    // todo: 상태 세분화 필요
+    HEALTH,
+    ECOMMERCE,
+    FINANCE,
+    EDUCATION,
+    ENTERTAINMENT,
+    ETC
+}
