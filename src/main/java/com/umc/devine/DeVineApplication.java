1e6599e58720585b4e8f15aec5c9a23fe537b763
strfunctionk <comzun01@gmail.com>
2026-01-11 09:27:12 +0900

    Chore(main): @EnableJpaAuditing 추가
    IssueNum: #8

--- src/main/java/com/umc/devine/DeVineApplication.java
+++ src/main/java/com/umc/devine/DeVineApplication.java
@@ -3,6 +3,7 @@ package com.umc.devine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DeVineApplication {
 
