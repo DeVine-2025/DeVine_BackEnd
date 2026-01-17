63995ea37e103a90f0f93575453415a2901436f0
EunBi Ko <e_un_04@naver.com>
2026-01-17 23:57:07 +0900

    [Feat] 조회수 관리 스케줄러 추가 - 주간 조회수 리셋 로직 추가 - @EnableScheduling 설정 추가
    IssueNum #11

--- src/main/java/com/umc/devine/DeVineApplication.java
+++ src/main/java/com/umc/devine/DeVineApplication.java
@@ -2,9 +2,11 @@ package com.umc.devine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class DeVineApplication {
 
 	public static void main(String[] args) {
