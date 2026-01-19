ac5f50ed3a3b1908d86a3010e9a1465594cd5214
HEO YUJIN <81500596+yujin9907@users.noreply.github.com>
2026-01-20 01:25:28 +0900

    [CHORE] 스웨거 문서 공유 및 배포 테스트를 위한 build deploy 관련 수정 (#37)
    * [Chore] CD docker v2 적용 컨테이너 이미지 제거 로직
    
    - docker v2 업데이트 (docker-compose -> docker compose)
    - 기존 컨테이너를 명확히 중지 및 삭제 후 시작
    
    IssueNum #36
    
    * [Chore] 배포 구성을 위한 임시 설정 및 헬스체크
    
    - 보안 설정: Swagger 문서, Actuator 헬스 체크를 위해 시큐리티 인증을 비활성화하고 접근 허용 (향후 인증 로직 추가 시 수정 예정)
    - Dockerfile: 컨테이너 내부에서 네트워크 요청 및 디버깅에 활용될 수 있도록 curl, wget 유틸리티를 추가
    - application-prod.yml :  Actuator health endpoint 설정 추가
    
    IssueNum #36
    
    * [Feat] 배포 관련 테스트 환경 설정 수정
    
    - test : static 테스트 컨테이너 초기화 방식을 정적 블록으로 변경
    - test : PostgreSQL 및 Valkey 컨테이너의 연결 정보를 명시적 주입
    - Project : eta @Builder.Default를 추가 (실행 시 warn 수정)
    
    IssueNum #36
    
    * [Chore] 임시 시큐리티 주석, 공통 테스트 코드 누락 추가
    
    - withReuse(true) 누락 추가
    - 시큐리티 주석 추가
    
    IssueNum #36
    
    * [Fix] 스웨거 의존 버전 수정 (2.3.0 -> 2.8.13)
    
    ---------
    
    Co-authored-by: heo <qetyop2679@gmail.com>

--- src/test/java/com/umc/devine/DeVineApplicationTests.java
+++ src/test/java/com/umc/devine/DeVineApplicationTests.java
@@ -2,9 +2,7 @@ package com.umc.devine;
 
 import com.umc.devine.support.IntegrationTestSupport;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DeVineApplicationTests extends IntegrationTestSupport {
 
 	@Test
