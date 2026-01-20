f6ea06133adb86702ee006bf55081782290ab8ea
strfunctionk <84651690+strfunctionk@users.noreply.github.com>
2026-01-20 12:10:05 +0900

    [CHORE] techstack 오타 수정 (#41)
    * [Chore] typo techstack
    
    - teckstack 오타 techstack으로 변경
    
    IssueNum #28
    
    * [Chore] TechName 세분화
    
    - Python, React, Django, flask 추가
    
    IssueNum #28

--- src/main/java/com/umc/devine/domain/techstack/enums/TechName.java
+++ src/main/java/com/umc/devine/domain/techstack/enums/TechName.java
@@ -2,5 +2,5 @@ package com.umc.devine.domain.techstack.enums;
 
 public enum TechName {
     // todo: 세분화 필요
-    JAVA, JAVASCRIPT, CSS, SPRING
+    JAVA, JAVASCRIPT, CSS, SPRING, PYTHON, REACT, DJANGO, FLASK
 }
