6d26abd9173a216785133f75c274cf5fcd76f790
woojoo48 <wjddnwn0408@gmail.com>
2026-02-04 11:24:44 +0900

    [Feat] 회원가입 서비스 메서드 개발
    1. 가입된 회원인지 확인 후 없다면 회원가입 이동
    2. 약관 검증 및 동의 확인 검증
    3. 관심 도메인 유효성 검증
    4. 기술 스택 유효성 검증
    
    각 검증 완료 후 DB에 저장
    
    IssueNum #56

--- src/main/java/com/umc/devine/domain/member/service/command/MemberCommandService.java
+++ src/main/java/com/umc/devine/domain/member/service/command/MemberCommandService.java
@@ -4,8 +4,10 @@ import com.umc.devine.domain.member.dto.MemberReqDTO;
 import com.umc.devine.domain.member.dto.MemberResDTO;
 import com.umc.devine.domain.member.entity.Member;
 import com.umc.devine.domain.techstack.dto.TechstackResDTO;
+import com.umc.devine.global.auth.ClerkPrincipal;
 
 public interface MemberCommandService {
+    MemberResDTO.SignupResultDTO signup(ClerkPrincipal principal, MemberReqDTO.SignupDTO dto);
     MemberResDTO.MemberProfileDTO updateMember(Member member, MemberReqDTO.UpdateMemberDTO dto);
     TechstackResDTO.DevTechstackListDTO addMemberTechstacks(Member member, MemberReqDTO.AddTechstackDTO dto);
     TechstackResDTO.DevTechstackListDTO removeMemberTechstacks(Member member, MemberReqDTO.RemoveTechstackDTO dto);
