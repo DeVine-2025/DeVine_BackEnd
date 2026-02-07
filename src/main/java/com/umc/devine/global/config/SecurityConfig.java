241d76625488a3bbcdf1466bfd6388144a8aab86
strfunctionk <comzun01@gmail.com>
2026-02-07 18:37:44 +0900

    [Chore] 토큰 발급 페이지 제작 (개발용)
    - SecurityConfig 정적 페이지 허용
    
    - DevController 추가 (index.html 매핑용)
    
    - /dev/index.html 토큰 페이지 추가
    
    IssuNum #92

--- src/main/java/com/umc/devine/global/config/SecurityConfig.java
+++ src/main/java/com/umc/devine/global/config/SecurityConfig.java
@@ -62,6 +62,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/v1/projects/weekly-best").permitAll()
                         // 리포트 콜백 (임시) // TODO 프론트 json 확인을 위해서 임시 활성화, 마스터키로 인증 변경
                         .requestMatchers(HttpMethod.POST, "/api/v1/reports/callback").permitAll()
+                        // 정적 리소스 (테스트 용) // TODO : 추후 제거하기
+                        .requestMatchers("/", "/index.html", "/dev/**", "/static/**", "/*.js", "/*.css").permitAll()
                         // 그 외 모든 요청은 인증 필요
                         .anyRequest().authenticated()
                 )
