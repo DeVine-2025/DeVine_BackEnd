a47a88f3079e2242e6784e21278e4d10becbbd87
sunm2n <sunmin8410@naver.com>
2026-02-09 02:51:13 +0900

    [Feat] 프로젝트 임베딩 구현
    IssueNum #85

---
+++ src/main/java/com/umc/devine/domain/project/repository/ProjectEmbeddingRepository.java
@@ -0,0 +1,17 @@
+package com.umc.devine.domain.project.repository;
+
+import com.umc.devine.domain.project.entity.ProjectEmbedding;
+import com.umc.devine.global.enums.EmbeddingStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProjectEmbeddingRepository extends JpaRepository<ProjectEmbedding, Long> {
+
+    Optional<ProjectEmbedding> findByProjectId(Long projectId);
+
+    boolean existsByProjectId(Long projectId);
+
+    List<ProjectEmbedding> findByStatusAndRetryCountLessThan(EmbeddingStatus status, Integer maxRetryCount);
+}
