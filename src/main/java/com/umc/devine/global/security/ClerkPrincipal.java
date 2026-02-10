59ceea6d27828aa09079ae9c449779c4916f5c1d
sunm2n <sunmin8410@naver.com>
2026-02-10 17:26:25 +0900

    [Chore] auth -> security로 폴더 명 변경
    IssueNum #123

---
+++ src/main/java/com/umc/devine/global/security/ClerkPrincipal.java
@@ -0,0 +1,27 @@
+package com.umc.devine.global.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.security.Principal;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ClerkPrincipal implements Principal {
+
+    private final String clerkId;
+    private final String email;
+    private final String name;
+    private final String imageUrl;
+
+    @Override
+    public String getName() {
+        return clerkId;
+    }
+
+    public String getFullName() {
+        return name;
+    }
+}
