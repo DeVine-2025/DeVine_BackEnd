24620462b0769075930da3a0044c7666c0d6429e
strfunctionk <comzun01@gmail.com>
2026-02-11 20:53:57 +0900

    [Chore] 테스트 로그 추가
    IssueNum #155

--- src/main/java/com/umc/devine/global/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/umc/devine/global/security/CustomAuthenticationEntryPoint.java
@@ -6,6 +6,7 @@ import com.umc.devine.global.apiPayload.code.GeneralErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -24,6 +26,8 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
+
+        log.error("테스트 ✅", authException);
 
         GeneralErrorCode errorCode = GeneralErrorCode.UNAUTHORIZED;
 
